ced0d03195f65ae582feae86a0594aa3ade4aa5a
LeoSilva91 <167141107+LeoSilva91@users.noreply.github.com>
2024-08-09 14:59:03 -0300

    setAttribute

---
+++ Aula-10-agosto-2024/assets/js/script.js
@@ -0,0 +1,12 @@
+// Seleciona o botão pelo ID
+var btnCadastrar = document.getElementById("btnCadastrar");
+
+// Modifica o texto do botão
+btnCadastrar.textContent = "Novo Texto";
+
+// Modifica a classe do botão
+btnCadastrar.setAttribute("class", "btn btn-primary");
+
+// Se preferir, pode usar classList para adicionar ou remover classes
+// btnCadastrar.classList.remove("btn-success");
+// btnCadastrar.classList.add("btn-primary");
